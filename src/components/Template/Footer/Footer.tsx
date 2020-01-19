713da0d61fe127409882a96679e3a0f7c2e6cffa
jiwoohong <uzihoon.dev@gmail.com>
2020-01-19 17:51:53 +0900

    posting

--- src/components/Template/Footer/Footer.tsx
+++ src/components/Template/Footer/Footer.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 import classNames from "classnames/bind";
+import { useStatusGet } from "hooks/lib";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 function Footer() {
+  const admin = useStatusGet("admin") as boolean;
+
   return (
     <div className={cx("footer-wrapper")}>
       <a href="https://github.com/Uzihoon">
         © 2020 Uzihoon All rights reserved
       </a>
+      {admin && (
+        <div className={cx("oper-box")}>
+          <Link to="/uzihoon/admin/write">Posting</Link>
+        </div>
+      )}
     </div>
   );
 }
